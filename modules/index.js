786ba1e0b7590717c007719399df90ee5bf3171f
Andrei Gabreanu <andrei.gabreanu@middleout.com>
2016-03-20 14:12:24 +0200

    + Allows strict nesting via [parent].[name] notation. This way you can have routes with the same name and not collide with each other. The downside is that your main route (the "/" base route) needs to have a name. + Allows auto route names based on path if no name is present on a route. + Validation of names not to contain slashes or parameters, because a route name can be based on its path.

--- modules/index.js
+++ modules/index.js
@@ -17,19 +17,15 @@ const namedRoutesContextType = {
   }).isRequired
 }
 
-const createRouteMap = (routes, accumulatedPath='', map={}) => {
-  React.Children.forEach(routes, (route) => {
-    const slash = accumulatedPath === '/' ? '' : '/'
-    // can't type check because we might have a different IndexRoute in node_modules
-    const isIndexRoute = route.type.displayName === 'IndexRoute'
-    const path = isIndexRoute ? accumulatedPath :
-      route.props.path === '/' ?
-        '/' : `${accumulatedPath}${slash}${route.props.path}`
-    if (route.props.name)
-      map[route.props.name] = path
-    if (route.props.children)
-      createRouteMap(route.props.children, path, map)
+const createRouteMap = (route, parentName, parentPath, map = {}) => {
+  map       = handleExtractRouteName(route, parentName, parentPath, map)
+  let routeName = getRouteName(route, parentName)
+  let routePath = getRoutePath(route, parentPath)
+
+  React.Children.forEach(route.props.children, (child) => {
+    map = createRouteMap(child, routeName, routePath, map)
   })
+
   return map
 }
 
@@ -81,3 +77,60 @@ export const NamedRouteLink = React.createClass({
 
 })
 
+/**
+ * @param route
+ * @param parentName
+ * @returns {any}
+ */
+function getRouteName(route, parentName) {
+  if ( !route.props.path && !route.props.name ) {
+    return parentName
+  }
+
+  let routeName = route.props.name || route.props.path
+
+  if ( !routeName ) {
+    return parentName
+  }
+
+  validateRouteName(routeName)
+  return parentName ? parentName + '.' + routeName : routeName
+}
+
+/**
+ * @param route
+ * @param parentPath
+ * @returns {any}
+ */
+function getRoutePath(route, parentPath) {
+  if ( !route.props.path ) {
+    return parentPath
+  }
+
+  return (parentPath ? parentPath + '/' + route.props.path : route.props.path).replace('//', '/')
+}
+
+/**
+ * @param name
+ */
+function validateRouteName(name) {
+  if ( name.indexOf(':') !== -1 || name.indexOf('*') !== -1 || name.indexOf('/') !== -1 ) {
+    if ( process.env.NODE_ENV !== 'production' ) {
+      throw new Error('Invalid route name. The route with name ' + name + ' contains parameters (since it contains ":", "*" or "/" thus the name is invalid. Please check this route.')
+    } else {
+      throw new Error('Invalid route name "' + name + '". Please compile for "development" to see the full error message')
+    }
+  }
+}
+
+function handleExtractRouteName(route, parentName, parentPath, map) {
+  let routeName = getRouteName(route, parentName)
+  let routePath = getRoutePath(route, parentPath)
+
+  if ( !routeName ) {
+    return map
+  }
+
+  map[ routeName ] = routePath
+  return map
+}
